3c47012ec202af475cae3081a4a7cb41f81231d9
Raygunito <112569656+Raygunito@users.noreply.github.com>
2024-03-31 23:52:08 +0200

    Create main page for Doxygen

---
+++ src/mainpage.h
@@ -0,0 +1,11 @@
+/**
+ * @mainpage Library Documentation
+ *
+ * @section intro_sec Introduction
+ *
+ * Welcome to the documentation for our library! This documentation is generated using Doxygen, and it provides a comprehensive guide to the functionality, usage, and implementation details of our library.
+ *
+ * @section files_sec File List
+ *
+ * For a complete list of files, refer to the <a href="files.html">File List</a> section.
+ */
