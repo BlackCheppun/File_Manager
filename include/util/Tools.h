718cb24be71ae5b61ed2fd109517f008b84a93ea
blackcheppun <aghouilesilyes1@gmail.com>
2025-04-02 12:58:38 +0200

    added functions related to directories for saving, gettingnname, and adding files

--- include/util/Tools.h
+++ include/util/Tools.h
@@ -1,8 +1,5 @@
 /**
  * @file Tools.h
- * @author Farah ALIANE
- * @author Laurent LIN
- * @brief 
  */
 
 #ifndef TOOLS_H
@@ -14,36 +11,36 @@
 
 /**
  * @brief Charge les informations du SuperBloc depuis PARTITION_NAME
- * 
+ *
  * @param sb adresse du superbloc
  * @return int 0 en cas de réussite | -1 sinon
  */
-int loadSuperBlock(SuperBlock* sb);
+int loadSuperBlock(SuperBlock *sb);
 /**
  * @brief Charge les informations du BlockBitmap depuis PARTITION_NAME
- * 
+ *
  * @param bbmp adresse du blockbitmap
  * @return int 0 en cas de réussite | -1 sinon
  */
-int loadBlockBitmap(BlockBitmap* bbmp);
+int loadBlockBitmap(BlockBitmap *bbmp);
 /**
  * @brief Charge les informations de FileBlock depuis PARTITION_NAME
- * 
+ *
  * @param fileArray adresse du tableau de file
  * @return int 0 en cas de réussite | -1 sinon
  */
 int loadFileBlock(File *fileArray);
 /**
  * @brief Charge les informations de DirBlock depuis PARTITION_NAME
- * 
+ *
  * @param dirArray adresse du tableau de Dossier
  * @return int 0 en cas de réussite | -1 sinon
  */
-int loadDirBlock(Directory* dirArray);
+int loadDirBlock(Directory *dirArray);
 
 /**
  * @brief Retourne le premier indice du tableau Bitmap qui est libre (pour écrire dessus)
- * 
+ *
  * @param bbmp BlockBitmap avec toutes les informations
  * @return int i si on trouve un bloc libre | -1 sinon
  */
@@ -53,72 +50,72 @@ int indexOfFreeBBMP(BlockBitmap bbmp);
  * @brief Retourne l'indice du tableau indiqué par posSeek du File
  * <br/> Par exemple si on a un fichier de 7000B, et que notre seek vaut 4500 alors,
  * on doit recevoir l'indice qui pointe sur le 2eme bloc utilisé par le fichier
- * 
- * @param file 
+ *
+ * @param file
  * @return int i si on trouve | -1 en cas d'erreur
  */
-int indexBBMPOfPosSeek(File* file);
+int indexBBMPOfPosSeek(File *file);
 /**
  * @brief Retourne l'indice du tableau indiqué par posSeek du File avec le BBMP de chargé
  * <br/> Par exemple si on a un fichier de 7000B, et que notre seek vaut 4500 alors,
  * on doit recevoir l'indice qui pointe sur le 2eme bloc utilisé par le fichier
- * 
- * @param file 
- * @param bbmp 
+ *
+ * @param file
+ * @param bbmp
  * @return int i si on trouve | -1 en cas d'erreur
  */
-int indexBBMPOfPosSeekLoaded(File* file,BlockBitmap bbmp);
+int indexBBMPOfPosSeekLoaded(File *file, BlockBitmap bbmp);
 /**
- * @brief Sauvegarde dans PARTITION_NAME l'information du SuperBlock 
- * 
- * @param sb 
+ * @brief Sauvegarde dans PARTITION_NAME l'information du SuperBlock
+ *
+ * @param sb
  * @return int 0 en cas de réussite | -1 sinon
  */
 int saveSuperBlock(SuperBlock sb);
 /**
  * @brief Sauvegarde dans PARTITION_NAME l'information d'un File à sa position précise de FileBlock
- * 
- * @param file 
- * @param index 
+ *
+ * @param file
+ * @param index
  * @return int 0 en cas de réussite | -1 sinon
  */
-int saveFileBlock(File file,int index);
+int saveFileBlock(File file, int index);
 /**
  * @brief Sauvegarde dans PARTITION_NAME l'information du Bitmap
- * 
+ *
  * @param bbmp Tableau d'indice des zones
  * @return int 0 en cas de réussite | -1 sinon
  */
 int saveBBMP(BlockBitmap bbmp);
 
 /**
- * @brief Affiche les informations du superbloc 
- * 
- * @param sb SuperBlock 
+ * @brief Affiche les informations du superbloc
+ *
+ * @param sb SuperBlock
  */
 void printSB(SuperBlock sb);
 /**
  * @brief Affiche le tableau de BlockBitmap pour montrer les zones occupés et libres
- * 
+ *
  * @param bbmp BlockBitmap qui est chargé par loadBBMP()
  */
 void printBBMP(BlockBitmap bbmp);
 /**
  * @brief Affiche les informations de tous les fichiers disponibles
- * 
+ *
  * @param array Tableau de fichier qu'on charge avec loadFileBlock
  */
 void printFILE(File array[]);
 /**
  * @brief Affiche le contenu (et de ses enfants) de tous les dossiers disponibles
- * 
+ *
  * @param array Tableau de dossier
  */
 void printDIR(Directory array[]);
 /**
  * @brief Affiche les informations des enfants du dossier
- * 
- * @param array Tableau d'entrée 
+ *
+ * @param array Tableau d'entrée
  * @param nbDirEnt nombre d'élement
  */
 void printDirEnt(DirectoryEntry array[], int nbDirEnt);
--- src/util/Tools.c
+++ src/util/Tools.c
@@ -9,9 +9,10 @@
 #include "../../include/data/TypeDossier.h"
 #include "../../include/data/TypeBitmap.h"
 
-int indexOfFreeBBMP(BlockBitmap bbmp){
-    int i= 0;
-    while (i< BLOCK_BITMAP_ARRAY_SIZE && bbmp.bmpTab[i] != 0)
+int indexOfFreeBBMP(BlockBitmap bbmp)
+{
+    int i = 0;
+    while (i < BLOCK_BITMAP_ARRAY_SIZE && bbmp.bmpTab[i] != 0)
     {
         i++;
     }
@@ -22,13 +23,15 @@ int indexOfFreeBBMP(BlockBitmap bbmp){
     return i;
 }
 
-int indexBBMPOfPosSeek(File* file){
+int indexBBMPOfPosSeek(File *file)
+{
     BlockBitmap bbmp;
     loadBlockBitmap(&bbmp);
-    return indexBBMPOfPosSeekLoaded(file,bbmp);
+    return indexBBMPOfPosSeekLoaded(file, bbmp);
 }
 
-int indexBBMPOfPosSeekLoaded(File* file,BlockBitmap bbmp){
+int indexBBMPOfPosSeekLoaded(File *file, BlockBitmap bbmp)
+{
     if (file->posSeek > file->size)
     {
         perror("cursor cannot exceed file size");
@@ -44,9 +47,9 @@ int indexBBMPOfPosSeekLoaded(File* file,BlockBitmap bbmp){
     return i;
 }
 
-
-int saveFileBlock(File f,int index){
-    int fd = open(PARTITION_NAME,O_RDWR);
+int saveFileBlock(File f, int index)
+{
+    int fd = open(PARTITION_NAME, O_RDWR);
     unsigned int tmp = f.posSeek;
     f.posSeek = 0;
     if (fd == -1)
@@ -54,12 +57,14 @@ int saveFileBlock(File f,int index){
         perror("save failed FileBlock");
         return -1;
     }
-    if (lseek(fd,FILEBLOCK_OFFSET + index*sizeof(File),SEEK_SET) == -1){
+    if (lseek(fd, FILEBLOCK_OFFSET + index * sizeof(File), SEEK_SET) == -1)
+    {
         close(fd);
         perror("save seek failed FileBlock");
         return -1;
     }
-    if (write(fd,&f,sizeof(f)) == -1){
+    if (write(fd, &f, sizeof(f)) == -1)
+    {
         close(fd);
         perror("save write failed FileBlock");
         return -1;
@@ -69,67 +74,102 @@ int saveFileBlock(File f,int index){
     return 0;
 }
 
-int saveSuperBlock(SuperBlock sb){
-    int fd = open(PARTITION_NAME,O_RDWR);
+int saveSuperBlock(SuperBlock sb)
+{
+    int fd = open(PARTITION_NAME, O_RDWR);
     if (fd == -1)
     {
         perror("save failed SuperBlock");
         return -1;
     }
 
-    if (write(fd,&sb,sizeof(sb)) == -1){
+    if (write(fd, &sb, sizeof(sb)) == -1)
+    {
         close(fd);
         perror("save write failed SuperBlock");
         return -1;
     }
-    
+
     close(fd);
     return 0;
 }
 
-int saveBBMP(BlockBitmap bbmp){
-    int fd = open(PARTITION_NAME,O_RDWR);
+int saveBBMP(BlockBitmap bbmp)
+{
+    int fd = open(PARTITION_NAME, O_RDWR);
     if (fd == -1)
     {
         perror("save failed FileBlock");
         return -1;
     }
-    if (lseek(fd,BITMAPBLOCK_OFFSET,SEEK_SET) == -1){
+    if (lseek(fd, BITMAPBLOCK_OFFSET, SEEK_SET) == -1)
+    {
         close(fd);
         perror("save seek failed FileBlock");
         return -1;
     }
-    if (write(fd,&bbmp,sizeof(bbmp)) == -1){
+    if (write(fd, &bbmp, sizeof(bbmp)) == -1)
+    {
         close(fd);
         perror("save write failed FileBlock");
         return -1;
     }
-    
+
     close(fd);
     return 0;
 }
 
-int loadSuperBlock(SuperBlock* sb){
-    if (sb == NULL){
-        sb = (SuperBlock*) malloc(sizeof(SuperBlock));
+int saveDirBlock(Directory *dirArray)
+{
+    int fd = open(PARTITION_NAME, O_WRONLY);
+    if (fd == -1)
+    {
+        perror("Error opening partition for writing");
+        return -1;
+    }
+
+    if (lseek(fd, DIRBLOCK_OFFSET, SEEK_SET) == -1)
+    {
+        perror("Error seeking to Directory Block");
+        close(fd);
+        return -1;
+    }
+
+    if (write(fd, dirArray, sizeof(Directory) * MAX_DIR_AMOUNT) == -1)
+    {
+        perror("Error writing directories");
+        close(fd);
+        return -1;
+    }
+
+    close(fd);
+    return 0;
+}
+
+int loadSuperBlock(SuperBlock *sb)
+{
+    if (sb == NULL)
+    {
+        sb = (SuperBlock *)malloc(sizeof(SuperBlock));
     }
 
     int fd = open(PARTITION_NAME, O_RDONLY);
-    
+
     if (fd == -1)
     {
         perror("erreur lecture SuperBlock");
         return -1;
     }
-    
+
     unsigned short sbVal[7];
-    
-    if((read(fd,sbVal,sizeof(sbVal))) < sizeof(sbVal)){
+
+    if ((read(fd, sbVal, sizeof(sbVal))) < sizeof(sbVal))
+    {
         close(fd);
         perror("error load value SuperBlock");
         return -1;
     }
-    
+
     sb->totalBlock = sbVal[0];
     sb->tailleBlock = sbVal[1];
     sb->nbBlockDispo = sbVal[2];
@@ -137,100 +177,150 @@ int loadSuperBlock(SuperBlock* sb){
     sb->nbFileDispo = sbVal[4];
     sb->totalDirectory = sbVal[5];
     sb->nbDirectoryDispo = sbVal[6];
-    
+
     char fs_name[4];
-    
-    if((read(fd,fs_name,sizeof(fs_name))) < sizeof(fs_name)){
+
+    if ((read(fd, fs_name, sizeof(fs_name))) < sizeof(fs_name))
+    {
         close(fd);
         perror("error load fs_name SuperBlock");
         return -1;
     }
-    
+
     for (int i = 0; i < 4; i++)
     {
         sb->fs_name[i] = fs_name[i];
     }
-    
+
     close(fd);
     return 0;
 }
-int loadBlockBitmap(BlockBitmap* bbmp){
+
+int loadBlockBitmap(BlockBitmap *bbmp)
+{
     if (bbmp == NULL)
     {
-        bbmp = (BlockBitmap*) malloc(sizeof(BlockBitmap));
+        bbmp = (BlockBitmap *)malloc(sizeof(BlockBitmap));
     }
-    
+
     int fd = open(PARTITION_NAME, O_RDONLY);
-    
+
     if (fd == -1)
     {
         perror("erreur lecture BlockBitmap");
         return -1;
     }
-    
-    if (lseek(fd,BITMAPBLOCK_OFFSET,SEEK_SET) == -1){
+
+    if (lseek(fd, BITMAPBLOCK_OFFSET, SEEK_SET) == -1)
+    {
         close(fd);
         perror("erreur seek blockbitmap");
         return -1;
     }
 
     unsigned short val;
-    
+
     for (int i = 0; i < BLOCK_BITMAP_ARRAY_SIZE; i++)
     {
-        if (read(fd,&val,sizeof(val)) == -1){
+        if (read(fd, &val, sizeof(val)) == -1)
+        {
             close(fd);
             return -1;
         }
         bbmp->bmpTab[i] = val;
     }
-    
+
     close(fd);
     return 0;
 }
-int loadFileBlock(File *fileArray){
+int loadFileBlock(File *fileArray)
+{
 
     int fd = open(PARTITION_NAME, O_RDONLY);
-    
+
     if (fd == -1)
     {
         perror("erreur lecture SuperBlock");
         return -1;
     }
 
-    if (lseek(fd,FILEBLOCK_OFFSET,SEEK_SET) == -1){
+    if (lseek(fd, FILEBLOCK_OFFSET, SEEK_SET) == -1)
+    {
         close(fd);
         perror("error seek load Fileblock");
         return -1;
     }
 
-    for (int i = 0; i < NUMBER_OF_BLOCK; i++){
+    for (int i = 0; i < NUMBER_OF_BLOCK; i++)
+    {
         File tmpF;
-        read(fd,&tmpF,sizeof(File));
+        read(fd, &tmpF, sizeof(File));
         fileArray[i] = tmpF;
     }
 
     close(fd);
     return 0;
 }
-int loadDirBlock(Directory* dirArray){
+// int loadDirBlock(Directory* dirArray){
+//     int fd = open(PARTITION_NAME, O_RDONLY);
+
+//     if (fd == -1)
+//     {
+//         perror("erreur lecture SuperBlock");
+//         return -1;
+//     }
+
+//     if (lseek(fd,DIRBLOCK_OFFSET,SEEK_SET) == -1){
+//         close(fd);
+//         perror("error seek load Fileblock");
+//         return -1;
+//     }
+
+//     for (int i = 0; i < MAX_DIR_AMOUNT; i++){
+//         Directory tmpD;
+//         read(fd,&tmpD,sizeof(Directory));
+//         dirArray[i] = tmpD;
+//     }
+
+//     close(fd);
+//     return 0;
+// }
+
+int loadDirBlock(Directory *dirArray)
+{
     int fd = open(PARTITION_NAME, O_RDONLY);
-    
     if (fd == -1)
     {
-        perror("erreur lecture SuperBlock");
+        perror("Error opening partition");
         return -1;
     }
 
-    if (lseek(fd,DIRBLOCK_OFFSET,SEEK_SET) == -1){
+    if (lseek(fd, DIRBLOCK_OFFSET, SEEK_SET) == -1)
+    {
         close(fd);
-        perror("error seek load Fileblock");
+        perror("Error seeking to Directory Block");
         return -1;
     }
 
-    for (int i = 0; i < MAX_DIR_AMOUNT; i++){
+    SuperBlock sb;
+    loadSuperBlock(&sb); // Load SuperBlock to validate
+
+    for (int i = 0; i < sb.totalDirectory; i++)
+    {
         Directory tmpD;
-        read(fd,&tmpD,sizeof(Directory));
+        if (read(fd, &tmpD, sizeof(Directory)) != sizeof(Directory))
+        {
+            perror("Error reading Directory");
+            close(fd);
+            return -1;
+        }
+
+        // Validate repoID (if needed)
+        if (tmpD.repoID == 0 && i != 0)
+        { // repoID=0 reserved for root
+            continue;
+        }
+
         dirArray[i] = tmpD;
     }
 
@@ -238,51 +328,167 @@ int loadDirBlock(Directory* dirArray){
     return 0;
 }
 
-void printSB(SuperBlock sb){
+void printSB(SuperBlock sb)
+{
     printf("SuperBlock infos :\n");
-    printf("\tTotal blocks : %u\n",sb.totalBlock);
-    printf("\tSize of block : %u\n",sb.tailleBlock);
-    printf("\tRemaining blocks : %u\n",sb.nbBlockDispo);
-    printf("\tTotal files : %u\n",sb.totalFile);
-    printf("\tRemaining files : %u\n",sb.nbFileDispo);
-    printf("\tTotal directories : %u\n",sb.totalDirectory);
-    printf("\tRemaining directories : %u\n",sb.nbDirectoryDispo);
-    printf("\tProprietary file system : %c%c%c\n",sb.fs_name[0],sb.fs_name[1],sb.fs_name[2]);
+    printf("\tTotal blocks : %u\n", sb.totalBlock);
+    printf("\tSize of block : %u\n", sb.tailleBlock);
+    printf("\tRemaining blocks : %u\n", sb.nbBlockDispo);
+    printf("\tTotal files : %u\n", sb.totalFile);
+    printf("\tRemaining files : %u\n", sb.nbFileDispo);
+    printf("\tTotal directories : %u\n", sb.totalDirectory);
+    printf("\tRemaining directories : %u\n", sb.nbDirectoryDispo);
+    printf("\tProprietary file system : %c%c%c\n", sb.fs_name[0], sb.fs_name[1], sb.fs_name[2]);
 }
-void printBBMP(BlockBitmap bbmp){
+void printBBMP(BlockBitmap bbmp)
+{
     printf("BlockBitmap infos :\n");
     int cpt = 0;
-    printf("L%d:\t",cpt++);
+    printf("L%d:\t", cpt++);
     for (int i = 0; i < BLOCK_BITMAP_ARRAY_SIZE; i++)
     {
-        printf("'%u'",bbmp.bmpTab[i]);
-        if ((i+1)%(BLOCK_BITMAP_ARRAY_SIZE/64) == 0 && cpt != 64)printf("\nL%d:\t",cpt++);
+        printf("'%u'", bbmp.bmpTab[i]);
+        if ((i + 1) % (BLOCK_BITMAP_ARRAY_SIZE / 64) == 0 && cpt != 64)
+            printf("\nL%d:\t", cpt++);
     }
     printf("\n");
 }
-void printFILE(File array[]){
+void printFILE(File array[])
+{
     printf("FileBlock infos :\n");
     for (int i = 0; i < NUMBER_OF_BLOCK; i++)
     {
-        printf("\tF%d:\t",i);
-        printf("'%s', %uB, Index %u\n",array[i].nom,array[i].size,array[i].posInBlockBMP);
+        printf("\tF%d:\t", i);
+        printf("'%s', %uB, Index %u\n", array[i].nom, array[i].size, array[i].posInBlockBMP);
     }
-    
 }
-void printDIR(Directory array[]){
-    printf("FileBlock infos :\n");
-    for (int i = 0; i < MAX_DIR_AMOUNT; i++)
+// void printDIR(Directory array[])
+// {
+//     printf("FileBlock infos :\n");
+//     for (int i = 0; i < MAX_DIR_AMOUNT; i++)
+//     {
+//         printf("\tD%d:\t", i);
+//         printf("'%s',Entries %u, Parent Index %u\n", array[i].nomDossier, array[i].nbEntry, array[i].parentIndex);
+//         printDirEnt(array[i].dirEnt, array[i].nbEntry);
+//     }
+// }
+
+void printDIR(Directory array[])
+{
+    printf("Directory Block Contents:\n");
+
+    // First load ALL files into memory
+    File allFiles[NUMBER_OF_BLOCK];
+    if (loadFileBlock(allFiles) == -1)
     {
-        printf("\tD%d:\t",i);
-        printf("'%s',Entries %u, Parent Index %u\n",array[i].nomDossier,array[i].nbEntry,array[i].parentIndex);
-        printDirEnt(array[i].dirEnt,array[i].nbEntry);
+        printf("Failed to load files!\n");
+        return;
+    }
+
+    for (int i = 0; i < array->nbSubRepos; i++)
+    {
+        if (array[i].repoID == 0 && i != 0)
+            continue; // Skip empty slots
+
+        printf("\tD%d (ID: %u):\t'%s'\n", i, array[i].repoID, array[i].nomDossier);
+        printf("\t\tParent ID: %u\n", array[i].parentID);
+
+        // Print files in this directory
+        printf("\t\tFiles: ");
+        for (int j = 0; j < array[i].nbFiles; j++)
+        {
+            // Find each file by ID
+            for (int k = 0; k < NUMBER_OF_BLOCK; k++)
+            {
+                if (allFiles[k].index == array[i].files[j])
+                {
+                    printf("%s ", allFiles[k].nom);
+                    break;
+                }
+            }
+        }
+        printf("\n");
+
+        // Print subdirectories
+        printf("\t\tSubdirectories: ");
+        for (int j = 0; j < array[i].nbSubRepos; j++)
+        {
+            printf("%s ", array[array[i].subRepos[j]].nomDossier);
+        }
+        printf("\n");
     }
 }
 
-void printDirEnt(DirectoryEntry array[], int nbDirEnt){
+void printDirEnt(DirectoryEntry array[], int nbDirEnt)
+{
     for (int i = 0; i < nbDirEnt; i++)
     {
-        printf("\tisDir:%u, index:%u",(int)array[i].isDirectory,array[i].index);
+        printf("\tisDir:%u, index:%u", (int)array[i].isDirectory, array[i].index);
     }
     printf("\n");
+}
+
+/**
+ * Generates a unique directory ID (simple incremental counter).
+ * Note: In a real system, track this in SuperBlock or a bitmap.
+ */
+short generateUniqueRepoID()
+{
+    static short nextID = 1; // Start at 1 (0 is reserved for root)
+    return nextID++;
+}
+
+/** Helper function to find directory index by ID */
+int findDirIndex(unsigned short repoID, Directory *dirArray)
+{
+    for (int i = 0; i < MAX_DIR_AMOUNT; i++)
+    {
+        if (dirArray[i].repoID == repoID)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+// Gets current directory name
+const char *getCurrentDirName(short currentDirectoryID)
+{
+    const char *currentDirName = "?";
+    static Directory dirArray[MAX_DIR_AMOUNT];
+    if (loadDirBlock(dirArray) == 0)
+    {
+        for (int i = 0; i < MAX_DIR_AMOUNT; i++)
+        {
+            if (dirArray[i].repoID == currentDirectoryID)
+            {
+                currentDirName = dirArray[i].nomDossier;
+                break;
+            }
+        }
+    }
+    return currentDirName;
+}
+
+// Updates parent directory's file list
+int addFileToDirectory(short dirID, short fileID)
+{
+    Directory dirArray[MAX_DIR_AMOUNT];
+    if (loadDirBlock(dirArray) == -1)
+        return -1;
+
+    for (int i = 0; i < MAX_DIR_AMOUNT; i++)
+    {
+        if (dirArray[i].repoID == dirID)
+        {
+            if (dirArray[i].nbFiles >= MAX_DIR_AMOUNT)
+            {
+                printf("Directory full!\n");
+                return -1;
+            }
+            dirArray[i].files[dirArray[i].nbFiles++] = fileID;
+            return saveDirBlock(dirArray);
+        }
+    }
+    return -1;
 }
